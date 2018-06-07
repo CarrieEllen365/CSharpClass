eb127a35dbad7ca10db94ed03d91a29197ee6e60
Carrie Melander <CarrieEllen365@outlook.com>
2018-06-06 22:41:15 -0700

    Initial Commit. class and properties

---
+++ 21stCenturyStadium/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21stCenturyStadium
+{
+    /// <summary>
+    /// Member account
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Unique account number
+        /// for the account
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// Email address of the user
+        /// associated with the account
+        /// </summary>
+        public string EmailAddress { get; set; }
+        /// <summary>
+        /// Type of account user signed up for
+        /// </summary>
+        public string TypeOfAccount { get; set; }
+        #endregion
+    }
+}
